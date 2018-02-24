a921f082876b347fc09e7d2509ac86bbbf79b576
David Richardson <david@tankski.co.uk>
2018-02-24 14:28:29 +0000

    Initial commit

---
+++ warwick_gg/settings/production.example.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'l33th4x0rs'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'uwcs_warwickgg',
+        'USER': 'uwcs_warwickgg',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+try:
+    from .local import *
+except ImportError:
+    pass
