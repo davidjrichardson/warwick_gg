f6f08f894134d14c79a36945f711afa4527f41b4
David Richardson <david@tankski.co.uk>
2018-05-04 11:24:39 +0100

    Add allauth to apps

--- warwick_gg/urls.py
+++ warwick_gg/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from warwick_gg.views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
