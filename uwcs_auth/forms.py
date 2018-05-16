8f601a531a143c44f69217f5294f04dd49b33c8d
David Richardson <david@tankski.co.uk>
2018-05-16 11:42:55 +0100

    Start work on signup form

--- uwcs_auth/forms.py
+++ uwcs_auth/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
-    uni_id = forms.CharField(max_length=11)
+    uni_id = forms.CharField(max_length=11, help_text='If you are an associate member then use that ID number')
     nickname = forms.CharField(max_length=50, required=False)
 
     def signup(self, request, user):
