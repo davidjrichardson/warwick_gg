7a887df24a4c2f9a22f0b097d34c0f7d892835ef
David Richardson <david@tankski.co.uk>
2018-05-21 13:33:40 +0100

    Start work on the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
 
-class DashboardIndexView(View):
+class DashboardIndexView(View, LoginRequiredMixin):
     template_name = 'dashboard/dashboard_index.html'
 
     def get(self, request):
