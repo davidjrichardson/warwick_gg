965e82290dc3f5458868a6d645b5efccb3857411
David Richardson <david@tankski.co.uk>
2018-06-07 10:48:18 +0100

    Start tying the dashboard together

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,11 @@
+from datetime import timedelta
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import View
+
+from events.models import Event
 
 
 class DashboardIndexView(LoginRequiredMixin, View):
@@ -8,4 +13,16 @@ class DashboardIndexView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
 
     def get(self, request):
-        return render(request, self.template_name)
+        # TODO: This could cause problems when event 1 starts after 2 but 2's signups start before 1.
+        next_event = Event.objects.filter(end__gte=timezone.now()).order_by('start').first()
+        just_finished = Event.objects.filter(start__lt=timezone.now(),
+                                             end__gte=(timezone.now() - timedelta(days=2))).order_by('start').first()
+
+        # TODO: Get tournaments
+
+        ctx = {
+            'event': next_event,
+            'just_finished': just_finished,
+            'event_signups_open': next_event.signups_open(request.user) if next_event else False
+        }
+        return render(request, self.template_name, ctx)
