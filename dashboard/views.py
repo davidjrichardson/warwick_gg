0def2162e20cebebe551cd86d9dd700cd73f20e1
David Richardson <david@tankski.co.uk>
2019-04-30 11:39:46 +0100

    Refactor the dashboard to make sense
    Remove the 500 error too

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,23 +14,17 @@ class DashboardIndexView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
 
     def get(self, request):
-        # TODO: This could cause problems when event 1 starts after 2 but 2's signups start before 1.
         next_event = Event.objects.filter(end__gte=timezone.now()).order_by('start').first()
         just_finished = Event.objects.filter(start__lt=timezone.now(),
                                              end__gte=(timezone.now() - timedelta(days=2))).order_by('start').first()
+        tournaments = Tournament.objects.filter(start__gte=timezone.now()).order_by('start').all()
 
         # Clean out the django messages buffer
         _ = list(messages.get_messages(request))
-
-        tournaments = tournaments = Tournament.objects.filter(start__gte=timezone.now()).order_by('start').all()
-
-        # TODO: Get tournaments
-
         ctx = {
             'event': next_event,
-            'event_signup_for_user': next_event.signup_start_for_user(request.user),
             'just_finished': just_finished,
-            'event_signups_open': next_event.signups_open(request.user) if next_event else False,
             'tournaments': tournaments
         }
+
         return render(request, self.template_name, ctx)
